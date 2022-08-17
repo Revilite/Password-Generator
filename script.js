071016e1509ae92d975f26990f9971b69a4ae512
Jacob Lockhart <procoder165@gmail.com>
2022-08-17 15:30:42 -0400

    adding nested if statements

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//declaration of characters
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",]
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ]
+var special = ["~", "`", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "\\", "|", "/", ".", ",",";", ":", "\"", "'", "<", ">", "?",  ]
+var lowerBool = false;
+var upperBool = false;
+var numberBool = false;
+var specialBool = false;
+
 var generatePassword = function(){
+     
+ // save for later 
   var numOfCharacters = window.prompt("How many characters?");
-
-  if (numOfCharacters >= 8 && numOfCharacters <= 128){
-     window.alert("yay");
-  }
-  else{
-    window.alert("noooo");
-  }
+  // var typeCheck = Math.floor(numOfCharacters);
+  // if (numOfCharacters < 8){
+  //    window.alert("Your number cannot be less than 8 characters");
+  // }
+  // else if (numOfCharacters > 128){
+  //   window.alert("Your number cannot be more than 128 characters");
+  // }
+  // else if (typeCheck === NaN){
+  //   window.alert("Your answer has to be a number");
+  // }
+  if (true){
   
+    var lowerChoice = window.confirm("Would you like Lowercase Characters?");
+    
+
+
+ var upperChoice = window.confirm("Would you like Uppercase Characters?");
+
+    if (upperChoice){
+      upperBool = true;
+    }
+    else{
+      upperBool = false;
+    }
 
 
 
-  return typeof(numOfCharacters);
+
+  }
+
+
+
+
+  return "hello World";
 }
 
 
