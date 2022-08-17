409f5a2edb9b0214c4763fff8551e29ef559d66c
Jacob Lockhart <procoder165@gmail.com>
2022-08-17 15:00:33 -0400

    created generate password function

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var generatePassword = function(){
+  var numOfCharacters = window.prompt("How many characters?");
+
+  if (numOfCharacters >= 8 && numOfCharacters <= 128){
+     window.alert("yay");
+  }
+  else{
+    window.alert("noooo");
+  }
+  
+
+
+
+  return typeof(numOfCharacters);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +46,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Create generatePassword function and put code in there
+
+
