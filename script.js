e5fd489c918919492be4d88e79706b6484ad256a
Jacob Lockhart <procoder165@gmail.com>
2022-08-18 13:59:32 -0400

    final tweaking

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//declaration of characters
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",]
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
-var special = ["~", "`", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "\\", "|", "/", ".", ",", ";", ":", "\"", "'", "<", ">", "?",]
-var lowerBool = false;
-var upperBool = false;
-var numberBool = false;
-var specialBool = false;
+
 
 var generatePassword = function () {
 
-  // save for later 
+  //declaration of characters
+  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
+  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",]
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
+  var special = ["~", "`", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "\\", "|", "/", ".", ",", ";", ":", "\"", "'", "<", ">", "?",]
+  var lowerBool = false;
+  var upperBool = false;
+  var numberBool = false;
+  var specialBool = false;
+
+
   var numOfCharacters = window.prompt("How many characters?");
   var typeCheck = parseInt(numOfCharacters);
 
@@ -92,20 +94,6 @@ var generatePassword = function () {
     return "";
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
